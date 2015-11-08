86db5d17f51230a0e1f3812a875e2e7e063fdb4c
Marouen Bousnina <bousnina.marouen@gmail.com>
2015-11-08 11:55:15 +0100

    fix unicron

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -33,6 +33,6 @@ end
 
 # Force the bundler gemfile environment variable to
 # # reference the capistrano "current" symlink
-# before_exec do |_|
-#   ENV['BUNDLE_GEMFILE'] = File.join(root, 'Gemfile')
-#   end
+ before_exec do |_|
+   ENV['BUNDLE_GEMFILE'] = File.join(root, 'Gemfile')
+ end
